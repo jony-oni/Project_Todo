7b5f1e1936fbd1836e2f8d7ec2aa551124cca176
DESKTOP-98BSQ6O\it <ghkdwjdgus99@naver.com>
2024-11-01 12:49:43 +0900

    1

---
+++ src/test/java/org/hjh/project_todo/ProjectTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hjh.project_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
